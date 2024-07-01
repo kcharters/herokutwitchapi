dfd463a781966c2f7fce973be742cbb8c1edc95b
Kirstin Charters <kirstin.charters@otago.ac.nz>
2024-07-02 08:13:42 +1200

    twitch api test

--- floral-scene-e6a4/src/entry.py
+++ floral-scene-e6a4/src/entry.py
@@ -1,10 +1,16 @@
-from twitchAPI.twitch import Twitch
 import asyncio
+from os import getenv
+
+from dotenv import load_dotenv
+from twitchAPI.twitch import Twitch
+
+load_dotenv()
 
 
 async def twitch_example():
 
-    twitch = await Twitch(env.CLIENT_ID, env.CLIENT_SECRET)
-
+    twitch: Twitch = await Twitch(getenv("CLIENT_ID"), getenv("CLIENT_SECRET"))
+    token =  twitch.get_app_token()
+    return token
 # run this example
 asyncio.run(twitch_example())
